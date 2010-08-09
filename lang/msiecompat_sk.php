d04300b04d42ff219aa3c673920d94d50be39d9b
salvatore@rezo.net <>
2010-08-09 09:20:56 +0000

    langues (msiecompat)

---
+++ lang/msiecompat_sk.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choix_explication' => '<NEW><p>Toto nastavenie zlep&#353;uje kompatibilitu str&aacute;nky s Internetom Explorer. </p>
+<ul>
+<li><a href=\'http://jquery.khurshid.com/ifixpng.php\'>iFixPng</a> (<b>predvolen&eacute; nastavenie</b>) obnov&iacute; poloprieh&#318;adnos&#357; PNG obr&aacute;zkov na MSIE&nbsp;5 a&nbsp;6. </li>
+<li><a href=\'http://code.google.com/p/ie7-js/\'>IE7.js</a> oprav&iacute; prieh&#318;adnos&#357; PNG a prid&aacute; selektory CSS2 pre MSIE&nbsp;5 a&nbsp;6 (<a href=\'http://ie7-js.googlecode.com/svn/test/index.html\'>tu je zoznam compatibiln&yacute;ch selectorov od IE7.js and IE8.js.</a>)</li>
+<li>IE8.js zlep&#353;uje IE7.js so selektormi CSS pre MSIE od 5 do&nbsp;7.</li>
+<li>IE7-squish opravil tri chyby v MSIE 6 (vr&aacute;tane dvojit&eacute;ho okraja pl&aacute;vaj&uacute;cich prvkoch), ale m&ocirc;&#382;u sa objavi&#357; in&eacute; probl&eacute;my. </li>
+</ul>',
+	'choix_non' => '<NEW>Neaktivova&#357;: ni&#269; neprid&aacute;va&#357; na moje str&aacute;nky',
+	'choix_titre' => '<NEW>Kompatibilita s Microsoft Internet Explorerom '
+);
+
+?>
