3833a0493dbfc892e23b1048a458d8e9a8d5ee86
salvatore@rezo.net <>
2010-10-05 20:31:17 +0000

    langues (msiecompat)

---
+++ lang/msiecompat_ar.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choix_explication' => '<p>Cette configuration vous permet d&rsquo;am&eacute;liorer la compatibilit&eacute; du site public avec le navigateur Internet Explorer.</p>
+			<ul>
+			<li><a href=\'http://jquery.khurshid.com/ifixpng.php\'>iFixPng</a> (<b>par d&#233;faut</b>) r&#233;tablit la semi-transparence les images au format PNG sous MSIE&nbsp;5&nbsp;et&nbsp;6.</li>
+			<li><a href=\'http://code.google.com/p/ie7-js/\'>IE7.js</a> corrige les images PNG et ajoute des s&eacute;lecteurs CSS2 pour MSIE&nbsp;5&nbsp;et&nbsp;6 (<a href=\'http://ie7-js.googlecode.com/svn/test/index.html\'>vous pouvez consulter la liste des s&#233;lecteurs compatibles introduits par IE7.js et IE8.js</a>).</li>
+			<li>IE8.js compl&#232;te IE7.js en enrichissant les comportements des CSS de MSIE 5 &#224;&nbsp;7. </li>
+			<li>IE7-squish corrige trois bugs de MSIE&nbsp;6 (notamment la double marge des &#233;l&#233;ments flottants), mais des effets ind&#233;sirables peuvent appara&#238;tre (le webmestre doit v&#233;rifier la compatibilit&#233;).</li>
+			</ul>', # NEW
+	'choix_non' => '&#1593;&#1583;&#1605; &#1575;&#1604;&#1578;&#1601;&#1593;&#1610;&#1604;: &#1593;&#1583;&#1605; &#1573;&#1590;&#1575;&#1601;&#1577; &#1575;&#1610; &#1588;&#1610;&#1569; &#1593;&#1604;&#1609; &#1575;&#1604;&#1589;&#1601;&#1581;&#1575;&#1578; &#1575;&#1604;&#1606;&#1605;&#1608;&#1584;&#1580;&#1610;&#1577;',
+	'choix_titre' => '&#1575;&#1604;&#1578;&#1608;&#1575;&#1601;&#1602; &#1605;&#1593; &#1605;&#1575;&#1610;&#1603;&#1585;&#1608;&#1587;&#1608;&#1601;&#1578; &#1575;&#1606;&#1578;&#1585;&#1606;&#1578; &#1575;&#1603;&#1587;&#1576;&#1604;&#1608;&#1585;&#1585;'
+);
+
+?>
