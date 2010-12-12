121b9e48a35675653c7f35e5993d071503f157a6
salvatore@rezo.net <>
2010-12-12 17:13:46 +0000

    langues (msiecompat)

---
+++ lang/msiecompat_it.php
@@ -0,0 +1,22 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choix_explication' => '<p>Questa configurazione ti consente di migliorare la compatibilit&agrave; del sito pubblico con il browser Internet Explorer.</p>
+   <ul>
+   <li><a href=\'http://jquery.khurshid.com/ifixpng.php\'>iFixPng</a> (<b>predefinito</b>) ristabilisce la trasparenza delle immagini nel formato PNG con MSIE&nbsp;5&nbsp;e&nbsp;6.</li>
+   <li><a href=\'http://code.google.com/p/ie7-js/\'>IE7.js</a> corregge le immagini PNG e aggiunge dei selettori CSS2 per MSIE&nbsp;5&nbsp;e&nbsp;6 (<a href=\'http://ie7-js.googlecode.com/svn/test/index.html\'>puoi consultare la lista dei selettori compatibili introdotti da IE7.js e IE8.js</a>).</li>
+   <li>IE8.js completa IE7.js arricchendo i comportamenti del CSS da MSIE 5 a&nbsp;7. </li>
+   <li>IE7-squish corregge tre bug di MSIE&nbsp;6 (in particolare il doppio margine degli elementi float), ma possono verificarsi degli effetti collaterali (il webmaster deve verificarne la compatibilit&agrave;).</li>
+   </ul>',
+	'choix_non' => 'Non attivare: non aggiungere niente ai miei modelli',
+	'choix_titre' => 'Compatibilit&agrave; Microsoft Internet Explorer'
+);
+
+?>
