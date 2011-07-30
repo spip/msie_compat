0a9db3de53207c1723c60fd6083475ae35bb13d5
eric@smellup.net <>
2011-07-30 14:52:13 +0000

    Début des tests du nouveau fichier paquet.xml. On ajoute pour chaque plugin du core : - le paquet.xml sous le nom _paquet.xml pour éviter une utilisation par défaut à la place de plugin.xml Il suffira de le renommer pour effectuer les tests. - les fichiers de langue nécessaire à la description et au slogan. - un fichier _paquet-migration qui contient les commandes svn pour migrer ou ajouter des fichiers. Ce fichier n'a plus d'autre intérêt que de lister les fichiers à renommer pour retrouver le fonctionnement des balises disparues : install, fonctions et options.

---
+++ lang/paquet-msie_compat_de.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-msie_compat
+// Langue: de
+// Date: 30-07-2011 15:01:37
+// Items: 1
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'msie_compat_description' => 'Bietet verschiedene Methoden für:
+-* transparente PNG Dateien unter MSIE~6 
+-* und /oder diverse CSS Selektoren für MSIE~6 und~7.',
+);
+?>
---
+++ lang/paquet-msie_compat_fr.php
@@ -0,0 +1,20 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-msie_compat
+// Langue: fr
+// Date: 30-07-2011 15:01:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'msie_compat_description' => 'Permet de sélectionner différentes méthodes 
+-* pour assurer l\'affichage des fichiers PNG transparents sous MSIE~6 
+-* et/ou activer certains sélecteurs CSS dans MSIE~6 et~7.',
+	'msie_compat_slogan' => 'PNG et sélecteurs CSS pour les vieux navigateurs',
+);
+?>
