be8636909c0998b21dd4e146bce3477c8fe0b700
kent1@arscenic.info <>
2012-01-04 09:38:25 +0000

    langues (paquet-msie_compat)

--- lang/paquet-msie_compat_en.php
+++ lang/paquet-msie_compat_en.php
@@ -11,7 +11,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'msie_compat_description' => 'Offers different javascript methods to add support for:
 -* transparent PNG files to MSIE~6 
 -* and/or various CSS selectors to MSIE~6 and~7.',
-	'msie_compat_nom' => 'Support vieux navigateurs', # NEW
+	'msie_compat_nom' => 'Support of older browsers',
 	'msie_compat_slogan' => 'PNG files and various CSS selectors for old browsers'
 );
 
