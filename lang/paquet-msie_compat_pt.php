1afbc7aaddccc126d668732dec612f471e2debd3
salvatore@rezo.net <>
2013-09-22 16:59:38 +0000

    langues (paquet-msie_compat)

--- lang/paquet-msie_compat_pt.php
+++ lang/paquet-msie_compat_pt.php
@@ -10,9 +10,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// M
 	'msie_compat_description' => 'Permite seleccionar diferentes métodos javascript para:
 
--* assegurar a exibição de ficheiros transparentes PNG em MSIE~6 
+-* assegurar a exibição de ficheiros transparentes PNG em MSIE 6 
 
--* e/ou activar vários selectores CSS para MSIE~6 e ~7.',
+-* e/ou activar vários selectores CSS para MSIE 6 e 7.',
 	'msie_compat_nom' => 'Suporte para navegadores antigos',
 	'msie_compat_slogan' => 'Ficheiros PNG e selectores CSS para os navegadores antigos'
 );
