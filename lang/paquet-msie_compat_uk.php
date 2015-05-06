f4c84862e4bd488627c98f73f209b5581051d3df
olly.olga@gmail.com <>
2015-05-06 03:33:43 +0000

    [Salvatore] [source:_core_/plugins/msie_compat/lang/ paquet-msie_compat] Export depuis http://trad.spip.net de la langue uk

---
+++ lang/paquet-msie_compat_uk.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-msie_compat?lang_cible=uk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'msie_compat_description' => 'Декілька різних javascript бібліотек, які забезпечують:
+
+-* підтримку прозорості PNG  для MSIE 6 
+
+-* підтримку ряду CSS селекторів для MSIE 6 і 7.',
+	'msie_compat_nom' => 'Підтримка старих браузерів',
+	'msie_compat_slogan' => 'Підтримка прозорості PNG і розширена підтримка селекторів CSS для старих браузерів'
+);
+
+?>
