22155f24e0528257b1e32609e918ea9643a11300
spip.franck@lien-d-amis.net <>
2017-05-08 11:46:54 +0000

    code.spip est maintenant en https, donc j'ajoute le "s" à http

--- action/configurer_iecompat.php
+++ action/configurer_iecompat.php
@@ -19,7 +19,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 include_spip('inc/config');
 
-// http://code.spip.net/@action_configurer_dist
+// https://code.spip.net/@action_configurer_dist
 function exec_configurer_iecompat() {
 	
 	$securiser_action = charger_fonction('securiser_action', 'inc');
