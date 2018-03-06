bb803549984305dbb1fe91294a5df7b8576bd1cb
gpl@long-player.net <>
2018-03-06 04:36:35 +0000

    [Salvatore] [source:_core_/plugins/msie_compat/lang/ msiecompat] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/msiecompat_eo.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/msiecompat?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'choix_explication' => 'Tiu agordo plibonigas la kongrueco de la publika retejo kun la foliumilo Internet Explorer.',
+	'choix_ie7' => '<a href=\'http://code.google.com/p/ie7-js/\'>IE7.js</a> korektas la PNG-bildojn kaj aldonas CSS2-elektilojn por MSIE 5 kaj 6 : (<a href=\'http://ie7-js.googlecode.com/svn/test/index.html\'>videblas la listo de la kongruaj elektiloj fare de IE7.js kaj IE8.js</a>).',
+	'choix_ie7squish' => 'IE7-squish korektas tri MSIE6-cimojn (precipe la duoblan marĝenon de glitaj objektoj), sed kromefikoj okazeblas (la retejestro kontrolu la kongruecon).',
+	'choix_ie8' => 'IE8.js suplementas IE7.js per riĉigo de la CSS el MSIE 5 ĝis 7.',
+	'choix_ifixpng' => 'Defaŭlta elekto. <a href=\'http://jquery.khurshid.com/ifixpng.php\'>iFixPng</a> refunkciigas la duon-travideblon de la PNG-bildoj en MSIE 5 kaj 6.',
+	'choix_non' => 'Ne uzi : nenion aldoni al miaj skeletoj',
+	'choix_titre' => 'Kongrueco kun Microsoft Internet Explorer'
+);
