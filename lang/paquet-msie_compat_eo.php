f4695467fffa356a429e9e980ff024d0a3df341b
gpl@long-player.net <>
2018-03-06 04:36:36 +0000

    [Salvatore] [source:_core_/plugins/msie_compat/lang/ paquet-msie_compat] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-msie_compat_eo.php
@@ -0,0 +1,20 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-msie_compat?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'msie_compat_description' => 'Ebligas elekti plurajn metodojn :
+
+-* por funkciigi travideblajn PNG-bildojn ĉe MSIE 6 
+
+-* kaj, laŭvole, funkciigi kelkajn CSS-elektilojn ĉe MSIE 6 kaj 7.',
+	'msie_compat_nom' => 'Subteno por malnovaj foliumiloj',
+	'msie_compat_slogan' => 'PNG kaj CSS-elektiloj por la malnovaj foliumiloj'
+);
